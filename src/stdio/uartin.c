b7ee18b48c5db3b80d0b5e9c8212de827775533e
Un1q32 <joey.t.reinhart@gmail.com>
2024-10-25 15:43:45 -0400

    update stdio

--- src/stdio/uartin.c
+++ src/stdio/uartin.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <uart.h>
 
-FILE _uartin = {-1, __SRD, NULL,      0,          0,    '\0', NULL,
-                0,  0,     uart_read, uart_write, NULL, NULL};
+FILE _uartin = {'\0', __SRD, -1,        NULL,       0,    0,   NULL,
+                0,    0,     uart_read, uart_write, NULL, NULL};
 
 FILE *uartin = &_uartin;
--- src/stdio/uartout.c
+++ src/stdio/uartout.c
@@ -3,7 +3,8 @@
 
 static char uartout_buf[BUFSIZ];
 
-FILE _uartout = {-1, __SWR | __SLBF, uartout_buf, BUFSIZ, 0,   '\0', NULL, 0,
-                 0,  uart_read,      uart_write,  NULL,   NULL};
+FILE _uartout = {'\0',       __SWR | __SLBF, -1,  uartout_buf, BUFSIZ,
+                 0,          NULL,           0,   0,           uart_read,
+                 uart_write, NULL,           NULL};
 
 FILE *uartout = &_uartout;
