95e3370614c08931e53fced22f3b9a01a5348950
Un1q32 <joey.t.reinhart@gmail.com>
2026-01-07 18:57:47 -0500

    malloc printf

--- src/stdio/vfprintf.c
+++ src/stdio/vfprintf.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int vfprintf(FILE *restrict stream, const char *restrict format, va_list ap) {
   va_list ap2;
   va_copy(ap2, ap);
-  char buf[vsnprintf(NULL, 0, format, ap2) + 1];
+  int bufsize = vsnprintf(NULL, 0, format, ap2);
   va_end(ap2);
-  int ret = vsnprintf(buf, sizeof(buf), format, ap);
+  if (bufsize == -1)
+    return -1;
+  char *buf = malloc(bufsize + 1);
+  if (!buf)
+    return -1;
+  int ret = vsnprintf(buf, bufsize + 1, format, ap);
   if (fwrite(buf, 1, ret, stream) != (size_t)ret)
     return -1;
+  free(buf);
   return ret;
 }
